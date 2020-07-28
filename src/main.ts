1f236d2e93fdd14bbadad170621629d229e44f20
Irina Lepekhina <irina.lepekhina95@gmail.com>
2020-07-28 23:16:29 +0300

    added movies api

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,8 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  created() {
+    this.$store.dispatch("loadMovies");
+  },
   render: h => h(App)
 }).$mount("#app");
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,33 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    movies: []
+  },
+  mutations: {
+    setMovies(state, movies) {
+      state.movies = movies;
+    }
+  },
+  actions: {
+    loadMovies({ commit }) {
+      axios
+        .get("./movie-data.json", {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => {
+          commit("setMovies", response.data);
+        })
+        .catch(error => {
+          //TODO
+        });
+    }
+  },
   modules: {}
 });
