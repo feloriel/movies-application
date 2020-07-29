51fd54af1c574deebc71a7c989e9dfb6d5a592bb
Irina Lepekhina <irina.lepekhina95@gmail.com>
2020-07-29 19:53:06 +0300

    setup vuetify and added header

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <span class="title ml-3 mr-5">Movies Application</span>
+    <v-spacer></v-spacer>
+    <v-text-field
+      solo-inverted
+      flat
+      hide-details
+      label="Search"
+      prepend-inner-icon="mdi-magnify"
+    ></v-text-field>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {}
+</script>
