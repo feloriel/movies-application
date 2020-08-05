3203b2ba7d2fa3a14f7fa5dc75131f28f86ec227
Irina Lepekhina <irina.lepekhina95@gmail.com>
2020-08-06 00:23:17 +0300

    fixed loading movie and routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,12 +17,15 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Movie.vue")
+      import(/* webpackChunkName: "movie" */ "../views/Movie.vue")
   }
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
 });
 
 export default router;
--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,10 +1,10 @@
 <template>
   <MovieDetail v-if="currentMovie !== null" :currentMovie="currentMovie" />
-  <h1 v-else>Loading</h1>
+  <div v-else>Loading</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import MovieDetail from "@/components/movie/MovieDetail.vue";
 
 @Component({
@@ -16,11 +16,18 @@ export default class Movie extends Vue {
   get currentMovie() {
     return this.$store.state.currentMovie;
   }
+  get movies() {
+    return this.$store.getters.filteredMovies;
+  }
   created() {
     this.setCurrentMovie();
   }
   setCurrentMovie() {
     this.$store.commit("setCurrentMovie", this.$route.params.key);
+  }
+  @Watch("movies")
+  onMoviesChanged() {
+    this.setCurrentMovie();
   }
 }
 </script>
